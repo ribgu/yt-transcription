e5010706ae27fbe3d7beb078475691efd2ee09d7
ribgu <gustavo.ribeiro@sptech.school>
2023-10-25 15:28:52 -0300

    searching by video id

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
-import { YoutubeTranscript } from "youtube-transcript";
-import express from "express";
-const app = express();
-const port = 3000;
-let text = "";
-app.use(express.json());
+import { YoutubeTranscript } from "youtube-transcript"
+import express from "express"
+const app = express()
+const port = 3000
+let text = ""
+app.use(express.json())
 
 const getTranscript = async (req, res) => {
-  const transcript = await YoutubeTranscript.fetchTranscript(
-    "https://www.youtube.com/watch?v=PJZQrI-6oTU"
-  );
+  const baseUrl = "https://www.youtube.com/watch?v="
+  const id = req.params.id
+  const url = baseUrl + id
+  const transcript = await YoutubeTranscript.fetchTranscript(url)
 
   for (let i = 0; i < transcript.length; i++) {
     text += transcript[i].text + " "
@@ -16,8 +17,8 @@ const getTranscript = async (req, res) => {
   res.status(200).send(text)
 };
 
-app.get(`/transcript`, getTranscript)
+app.get(`/transcript/:id`, getTranscript)
 
 app.listen(port, () => {
-  console.log(`App listening at http://localhost:${port}`);
-});
+  console.log(`App listening at http://localhost:${port}`)
+})
